23a6d1716166fa282935e05c79552659b88145d3
Daniel Miller <earthmark.miller@gmail.com>
2018-04-07 12:58:46 -0700

    Add project files.

---
+++ Chem/Program.cs
@@ -0,0 +1,15 @@
+﻿3using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chem
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
